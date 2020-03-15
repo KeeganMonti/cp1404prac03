20504a05ea10c4f873cf7762d16c8fdcb12ab95e
KeeganMonti <keegan.monti@my.jcu.edu.au>
2020-03-15 15:48:30 +1000

    Revert "generate random price increase and decrease"
    This reverts commit daf5e85b

--- prac_02/randoms.py
+++
@@ -1,7 +0,0 @@
-import random
-print(random.randint(5, 20))  # line 1, can be a random whole number from 5 to 20 both inclusive.
-print(random.randrange(3, 10, 2))  # line 2, prints an number between 3 and 10 inclusive, however has 2 spacing
-# between selecting numbers so can only select: 3,5,7,9.
-print(random.uniform(2.5, 5.5))  # line 3, prints a random decimal between 2.5 and 5.5 inclusive.
-
-print(random.randint(1, 100))
